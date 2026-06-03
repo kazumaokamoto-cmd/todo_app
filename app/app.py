8421a7873205d86c6442b63529b24c59988adb25
kazumaokamoto <kazumaokamoto1105@gmail.com>
2026-06-03 11:40:25 +0900

    ver2.1

--- app/app.py
+++ app/app.py
@@ -1,16 +1,19 @@
 #Flaskとrender_template（HTMLを表示させるための関数）をインポート
 from flask import Flask,render_template, request, redirect
 from . import func
-
+from pathlib import Path
 
 
 #Flaskオブジェクトの生成
 app = Flask(__name__)
-
+normal_db_path = Path('app/db/todo.csv')
 
 @app.route("/")
 def index():
-    todos = func.load_csv()
+    #weekly_todoの追加
+    func.weekly_add()
+   
+    todos = func.load_csv(normal_db_path)
     #todosは辞書のリスト 
 
 # todos = [こんなかんじ
@@ -28,17 +31,16 @@ def index():
 #     }
 # ]
 
-    
+#is_doneがfalseのものだけを抽出して残り日数を計算
     todos = [t for t in todos if not t.get("is_done")]
     todos = func.calc_days_left(todos)
 
-    
+#todoをカテゴリで分けて送る    
     A = [t for t in todos if t.get("category") == "個人用"]
     B = [t for t in todos if t.get("category") == "課題"]
     C = [t for t in todos if t.get("category") == "ロボコン"]
 
 
-    
     return render_template("index.html", A=A, B=B, C=C)
 
 
@@ -48,7 +50,7 @@ def add(category):
     title = request.form['title']
     deadline = request.form.get('deadline')
     #csvに追加
-    func.save_csv({'title':title, 'category':category, 'deadline':deadline, 'is_done':False})
+    func.save_csv({'title':title, 'category':category, 'deadline':deadline, 'is_done':False}, normal_db_path)
     print('csvを保存しました')
 
 
@@ -56,5 +58,6 @@ def add(category):
 
 @app.route('/complete/<int:todo_id>', methods=['POST'])
 def complete(todo_id):
-    func.complete_todo(todo_id)
+    func.complete_todo(todo_id, normal_db_path)
     return redirect('/')
+
--- app/func.py
+++ app/func.py
@@ -1,20 +1,24 @@
 import pandas as pd
 from pathlib import Path
-from datetime import date, datetime
+from datetime import date, datetime, timedelta
+
+day_of_week = ['月', '火', '水', '木', '金', '土', '日']
+normal_db_path = Path('app/db/todo.csv')
+weekly_db_path = Path('app/db/weekly_todo.csv')
+
 '''
 save_csv
 はadd_todoをcsvファイルに追記する関数
 add_todoとはid, title, category, deadline, is_doneからなる辞書
  
 '''
-def save_csv(add_todo):
+def save_csv(add_todo, normal_db_path):
     
-    db_path = Path('app/db/todo.csv')
-    if db_path.is_file():
+    if normal_db_path.is_file():
         
         print('csvファイルが見つかりました')
         #csvファイルがあってデータがないなら新しいidをつくる(id=1)
-        df = pd.read_csv(db_path)
+        df = pd.read_csv(normal_db_path)
         if len(df) == 0:
             new_id = 0
         else:
@@ -22,24 +26,23 @@ def save_csv(add_todo):
         
         add_todo['id'] = new_id
         df = pd.concat([df, pd.DataFrame([add_todo])])
-        df.to_csv(db_path, index=False)
+        df.to_csv(normal_db_path, index=False)
     else:
         print('csvファイルが見つかりません。csvを作成します')
         
         add_todo['id'] = 0
         df = pd.DataFrame([add_todo])
-        df.to_csv(db_path, index=False)
+        df.to_csv(normal_db_path, index=False)
 
 
 '''
 load_csvはcsvファイルが存在するか確かめてからcsvのデータを辞書のリストに変換する関数
 トップページをつくるために呼び出される
 '''
-def load_csv():
+def load_csv(normal_db_path):
     
-    db_path = Path('app/db/todo.csv')
-    if db_path.is_file():
-        df = pd.read_csv(db_path)
+    if normal_db_path.is_file():
+        df = pd.read_csv(normal_db_path)
         return df.to_dict(orient='records')
     else:
         return []
@@ -51,12 +54,11 @@ complete_todoはtodo_doneをfalseからtrueにする関数
 todoが完了ときに呼び出される
 '''
 
-def complete_todo(todo_id):
+def complete_todo(todo_id, normal_db_path):
     
-    db_path = Path('app/db/todo.csv')
-    df = pd.read_csv(db_path)
+    df = pd.read_csv(normal_db_path)
     df.loc[df['id'] == todo_id, 'is_done'] = True
-    df.to_csv(db_path, index=False)
+    df.to_csv(normal_db_path, index=False)
 
 
 '''
@@ -76,3 +78,63 @@ def calc_days_left(todos):
         else:
             t['days_left'] = None
     return todos
+
+
+'''
+weekly_addは今日の曜日と同じ曜日のタスクをすべて追加する関数
+deadlineは一週間後になる
+'''
+
+def weekly_add():
+    today = date.today()
+    deadline = today + timedelta(7)
+    #曜日の番号=today.weekday()
+    weekly_todo = [#曜日のリスト
+        [#月曜日のリスト
+            {
+                'title':'現代文化論',
+                'category':'課題',
+                'deadline':None,
+                'is_done':False,
+            }
+        ],
+        [#火曜日のリスト
+            {
+                'title':'現代文化論',
+                'category':'課題',
+                'deadline':None,
+                'is_done':False,
+            },
+            {
+                'title':'情報学入門',
+                'category':'課題',
+                'deadline':None,
+                'is_done':False,
+            }
+        ],
+        [#水曜日のリスト
+            {
+                'title':'制御班',
+                'category':'ロボコン',
+                'deadline':None,
+                'is_done':False,
+            }
+        ]
+    
+        ]
+    print(f'今日は{day_of_week[today.weekday()]}曜日です')    
+        
+    #deadlineの設定したあとに保存する。この流れをforで回す
+    
+    for i in weekly_todo[today.weekday()]:
+        i['deadline'] = deadline
+        save_csv(i, normal_db_path)       
+        print('１つ保存した')
+    
+    
+
+
+
+
+
+
